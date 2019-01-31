ea313b28a8dffa08fa851c6b0edf4ddef9a11f3c
Aaron Chu <aaronchu357@gmail.com>
2019-01-31 19:52:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ function currentLine(line) {
     for (var i = 0; i < line.length; i++){
     var num = i + 1
     var output = `The line is currently: `
-    output += num
-    output += `. `
-    output += line[i]
   }
   }
-  return output
 }
